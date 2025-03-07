7087dbf9bf9189728e0bb4a47ac16d5f60e96ea2
CitruXonve <angelikaxu54@gmail.com>
2025-03-07 13:23:01 -0800

    feat: implement basic page sidebar w/ CSS support

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,66 @@
-import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-  } from "@remix-run/react";
-  
-  export default function App() {
-    return (
-      <html>
-        <head>
-          <link
-            rel="icon"
-            href="data:image/x-icon;base64,AA"
-          />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <h1>Remix CSR FE Boilerplate V1.0</h1>
+import { Form, Link, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+import React from "react";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <link rel="stylesheet" href=
+          "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+        </link>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <div className="sidebar-search">
+            <Form id="search-form" role="search">
+              <label className="fas fa-search search-input-icon"></label>
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder={"Search"}
+                type="search"
+              />
+              <div
+                aria-hidden
+                hidden={true}
+                id="search-spinner"
+              />
+            </Form>
+            <Form method="post">
+              <button type="submit" disabled>New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul className="sidebar-list">
+              <li className="sidebar-list-item">
+                <a href="#">Your Name</a>
+              </li>
+              <li className="sidebar-list-item">
+                <a href="#">Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+          <div id="badge">
+            <h4>Remix Contacts</h4>
+          </div>
+        </div>
+        <div id="detail">
           <Outlet />
-  
-          <Scripts />
-        </body>
-      </html>
-    );
-  }
-  
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
